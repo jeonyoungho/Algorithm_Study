494e509aabdaeea70c6df0b9c751e6715c98da76
jeon-yongho <yhjun1000@gmail.com>
2021-05-09 17:57:48 +0900

    update Template

--- Baekjoon/workspace/Template/src/main/java/Main.java
+++ Baekjoon/workspace/Template/src/main/java/Main.java
@@ -3,21 +3,25 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Stack;
 
 public class Main {
-	
-	public static void main(String[] args) throws NumberFormatException, IOException   {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
+
 		int N = Integer.parseInt(br.readLine());
-		
+
 		bw.write("");
 		bw.flush();
 		bw.close();
-		
+
 		br.close();
-		
+
 	}
-	
+
 }
