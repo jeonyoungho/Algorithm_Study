80478c040d115a6a37d1741f5f0450fbe57354ae
jeon-yongho <yhjun1000@gmail.com>
2021-06-03 18:00:24 +0900

    update Template

--- Baekjoon/workspace/Template/src/main/java/Main.java
+++ Baekjoon/workspace/Template/src/main/java/Main.java
@@ -3,10 +3,6 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Stack;
 
 public class Main {
 
