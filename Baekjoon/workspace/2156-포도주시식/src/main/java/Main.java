e1f2e4c3e95fc8636ce1de353f3ca1dbf8884958
jeon-yongho <yhjun1000@gmail.com>
2021-06-04 15:15:13 +0900

    update [2156]포도주시식.md

--- Baekjoon/workspace/2156-포도주시식/src/main/java/Main.java
+++ Baekjoon/workspace/2156-포도주시식/src/main/java/Main.java
@@ -19,8 +19,19 @@ public class Main {
 		}
 		
 		int[] dp = new int[n+1];
-		dp[1] = arr[1];
-		dp[2] = arr[1] + arr[2];
+		
+		if(n >= 1) {
+			dp[1] = arr[1];
+		}
+		
+		if(n >= 2) {
+			dp[2] = arr[1] + arr[2];
+		}
+		
+        if (n >= 3) {
+            dp[3] = Math.max(dp[2], Math.max(dp[1] + arr[3], arr[2] + arr[3]));
+        }
+		
 		for(int i=3;i<dp.length;i++) {
 			dp[i] = Math.max(dp[i-1], Math.max(dp[i-2] + arr[i], dp[i-3] + arr[i-1] + arr[i]));
 		}
